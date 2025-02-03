98752b05162eec7ae9a779b9bafc1725d91d0364
Michael Kamensky <mkamensk@ucsc.edu>
2025-02-02 18:49:24 -0800

    good basis for the memory class
    need to test it with console and make sure it compiles and works

---
+++ src/banana_memory.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+
+class BananaConsole; // change to the actual name of the console 
+
+class BananaMemory {
+    private:
+        BananaConsole * console;
+        uint8_t mem_array[0X10000]; // the size of array full adress sapce for the banana
+    public:
+        BananaMemory(BananaConsole * console, const std::string filename);
+        uint8_t l8u(uint16_t load_address) const;
+        uint16_t l16u(uint16_t load_address) const;
+        uint32_t loadInstruction(uint16_t load_address) const;
+        void w8u(uint16_t address, uint8_t value);
+        void w16u(uint16_t address, uint16_t value);
+        void writeInstrcution(uint16_t address, uint32_t value);
+};
