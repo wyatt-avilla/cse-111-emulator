1787763490b9fcca6a20e12168caaa5352f7156a
wyatt-avilla <wyattmurphy1@gmail.com>
2025-02-03 20:07:28 -0800

    console calls constructors for members with `this`

--- src/console.cpp
+++ src/console.cpp
@@ -2,7 +2,8 @@
 
 #include <fstream>
 
-Console ::Console(const std::string& slug_file_path) : cpu(CPU(this)) {
+Console ::Console(const std::string& slug_file_path)
+    : cpu(CPU(this)), os(OS(this)), memory(BananaMemory(this)) {
     std::ifstream slug_file(slug_file_path);
     if (!slug_file.is_open()) {
         throw std::invalid_argument(
