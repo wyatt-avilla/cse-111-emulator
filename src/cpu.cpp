8057aaa4bb046a7ed7f48d5b8fd21c9660d98085
Michelle <mgurovit@ucsc.edu>
2025-02-03 22:37:27 -0800

    Changed into clang format.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -99,7 +99,7 @@ void CPU::BEQ() {
 
 //void CPU::L16() {
     //registers[instruction_context.reg_b] =  this->console->memory.l16u(instruction_context.reg_a + instruction_context.immediate);
-//}
+//g}
 
 void CPU::L16() {
     registers[instruction_context.reg_b] = this->console->memory.l16u(
