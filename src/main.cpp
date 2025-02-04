63d360c5a8a25ec23eff81b021286caa94d86a22
wyatt-avilla <wyattmurphy1@gmail.com>
2025-02-04 11:28:03 -0800

    better exception handling in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,19 @@
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 
 int32_t main(int32_t argc, char* argv[]) {
     const int32_t expected_args = 1;
     if (argc - 1 != expected_args) {
         std::cerr << "Wrong number of command line arguments. (got " << argc - 1
-                  << ", expected" << expected_args << ")" << std::endl;
+                  << ", expected " << expected_args << ")" << std::endl;
+        exit(EXIT_FAILURE);
     }
 
     try {
         Console banana(argv[1]);
-    } catch (const std::invalid_argument& e) {
+    } catch (const std::exception& e) {
         std::cerr << "Couldn't initialize emulator {" << e.what() << "}"
                   << std::endl;
     }
