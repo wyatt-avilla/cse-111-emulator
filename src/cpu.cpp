a6b42e1aee40c3645605d1b7fae5f7b29e707452
wyatt-avilla <wyattmurphy1@gmail.com>
2025-02-05 15:04:49 -0800

    write 8 instead of 16

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -140,7 +140,7 @@ void CPU::S16() {
 void CPU::S8() {
     uint32_t effective_address =
         registers[instruction_context.reg_a] + instruction_context.immediate;
-    this->console->memory.w16u(
+    this->console->memory.w8u(
         effective_address,
         static_cast<uint8_t>(instruction_context.reg_b)
     );
