b36ed3215ce48fcfe8997a8b68707980918ebef7
Monisha Garika <150751194+MonishaG36912@users.noreply.github.com>
2025-02-10 20:09:49 -0800

    Update console.cpp

--- src/console.cpp
+++ src/console.cpp
@@ -5,11 +5,6 @@ Console ::Console() : cpu(CPU(this)), os(OS(this)), memory(Memory(this)) {}
 void Console::run(const std::string& slug_file_path) {
     os.reset(slug_file_path);
     while (true) {
-        os.loop();
-
-        // Check if the STOP address has been written to
-        if (memory.isStopAddressWritten()) {
-            break;
-        }
+        os.loop_iteration();
     }
 }
