4ea5539ddd7408c5e90a20a8a63aecb6a3151e97
Monisha Garika <150751194+MonishaG36912@users.noreply.github.com>
2025-02-10 19:52:53 -0800

    Update console.cpp
    Every time you loop in console, check if writing to STOP address

--- src/console.cpp
+++ src/console.cpp
@@ -4,5 +4,12 @@ Console ::Console() : cpu(CPU(this)), os(OS(this)), memory(Memory(this)) {}
 
 void Console::run(const std::string& slug_file_path) {
     os.reset(slug_file_path);
-    os.loop();
+    while (true) {
+        os.loop();
+
+        // Check if the STOP address has been written to
+        if (memory.isStopAddressWritten()) {
+            break;
+        }
+    }
 }
