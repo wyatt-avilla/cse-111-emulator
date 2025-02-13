5dda2b37847461852bd6ba899a3e1acc5d236043
Michelle <mgurovit@ucsc.edu>
2025-02-12 19:07:43 -0800

    added clang

--- src/console.cpp
+++ src/console.cpp
@@ -2,7 +2,7 @@
 
 Console ::Console() : cpu(CPU(this)), os(OS(this)) {}
 void Console::stopExecution() {
-    is_running = false;  // Set the flag to false to stop the execution loop
+    is_running = false; // Set the flag to false to stop the execution loop
 }
 void Console::run(const std::string& slug_file_path) {
     os.reset(slug_file_path);
