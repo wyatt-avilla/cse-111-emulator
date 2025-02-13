5cc70dc237f14f780f51c2d2a0c96d789c994ba7
Monisha Garika <150751194+MonishaG36912@users.noreply.github.com>
2025-02-12 18:52:44 -0800

    Update console.cpp

--- src/console.cpp
+++ src/console.cpp
@@ -1,10 +1,12 @@
 #include "console.h"
 
 Console ::Console() : cpu(CPU(this)), os(OS(this)) {}
-
+void Console::stopExecution() {
+    is_running = false;  // Set the flag to false to stop the execution loop
+}
 void Console::run(const std::string& slug_file_path) {
     os.reset(slug_file_path);
-    while (true) {
+    while (is_running) {
         os.loopIteration();
     }
 }
