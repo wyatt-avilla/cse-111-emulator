71a846a9b3fcce37667601cab7fa9a82ef14e008
Michelle <mgurovit@ucsc.edu>
2025-02-23 03:12:10 -0800

    Return to Previous

--- src/console.h
+++ src/console.h
@@ -11,19 +11,18 @@
 class Console {
   public:
     Console();
-    virtual ~Console(); // Added virtual destructor for proper cleanup
     CPU cpu;
     OS os;
     Memory memory;
     GPU gpu;
     void pollInput(); // Update controller state
-    uint8_t getControllerState() const;  void run(const std::string& slug_file_path);
+    uint8_t getControllerState() const;
+
+    void run(const std::string& slug_file_path);
     void stopExecution();
     [[nodiscard]] bool isRunning() const;
 
   private:
     bool is_running{true};
     uint8_t controllerState;
-
-
 };
