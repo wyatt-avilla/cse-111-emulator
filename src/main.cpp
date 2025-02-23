01b9b299b59362854dcc1c1da804927139390d9a
Michelle <mgurovit@ucsc.edu>
2025-02-23 03:47:47 -0800

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,28 @@ const uint8_t EXPECTED_ARGS = 1;
 const uint8_t FILENAME_INDEX = 1;
 
 int32_t main(const int32_t argc, const char* argv[]) {
-    if (argc - 1 != EXPECTED_ARGS) {
-        std::cerr << "Wrong number of command line arguments. (got " << argc - 1
-                  << ", expected " << EXPECTED_ARGS << ")" << std::endl;
+    if (argc - 1 != 1) {
+        std::cerr << "Wrong number of command line arguments." << std::endl;
         exit(EXIT_FAILURE);
     }
 
+    std::cerr << " Initializing Banana Emulator..." << std::endl;
     Console banana;
 
     try {
-        // Initialize the OS
+        std::cerr << " Starting OS reset..." << std::endl;
         OS os(&banana);
 
-        // Reset the OS with the file from command line arguments
-        os.reset(argv[FILENAME_INDEX]);
+        std::cerr << " Loading SLUG file: " << argv[1] << std::endl;
+        os.reset(argv[1]);  // 🚨 Error likely happens here!
 
-        // Main loop
-        bool running = true;
-        while (running) {
-            // Poll for input (keyboard/controller)
+        std::cerr << " Entering main loop..." << std::endl;
+        while (banana.isRunning()) {
             banana.pollInput();
-
-            // Perform one iteration of the OS loop
             os.loopIteration();
         }
     } catch (const std::exception& e) {
-        std::cerr << "Couldn't run " << "\"" << argv[FILENAME_INDEX]
-                  << "\":" << std::endl
+        std::cerr << " Couldn't run " << "\"" << argv[1] << "\":" << std::endl
                   << "    " << e.what() << std::endl;
     }
 
