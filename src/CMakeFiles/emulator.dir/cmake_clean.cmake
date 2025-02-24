db2eb9c9c1285844ddd79c27055270a6bb12cd78
Monisha <Monisha36912@gmail.com>
2025-02-23 17:53:50 -0800

    contoller

---
+++ src/CMakeFiles/emulator.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emulator.dir/console.cpp.o"
+  "CMakeFiles/emulator.dir/console.cpp.o.d"
+  "CMakeFiles/emulator.dir/cpu.cpp.o"
+  "CMakeFiles/emulator.dir/cpu.cpp.o.d"
+  "CMakeFiles/emulator.dir/gpu.cpp.o"
+  "CMakeFiles/emulator.dir/gpu.cpp.o.d"
+  "CMakeFiles/emulator.dir/main.cpp.o"
+  "CMakeFiles/emulator.dir/main.cpp.o.d"
+  "CMakeFiles/emulator.dir/memory.cpp.o"
+  "CMakeFiles/emulator.dir/memory.cpp.o.d"
+  "CMakeFiles/emulator.dir/os.cpp.o"
+  "CMakeFiles/emulator.dir/os.cpp.o.d"
+  "emulator"
+  "emulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/console.cpp
+++ src/console.cpp
@@ -3,7 +3,8 @@
 Console ::Console() : cpu(CPU(this)), os(OS(this)), memory(Memory(this)) {}
 
 void Console::stopExecution() {
-    is_running = false; // Set the flag to false to stop the execution loop
+    is_running = false; 
+    //Set the flag to false to stop the execution loop
 }
 
 bool Console::isRunning() const { return is_running; }
