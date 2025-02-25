deecab27237ad93a9262f5bbbedcc9c5e70e769e
wyatt-avilla <wyattmurphy1@gmail.com>
2025-02-25 11:44:30 -0800

    fix controller lints

--- src/controller.cpp
+++ src/controller.cpp
@@ -4,54 +4,31 @@
 #include "console.h"
 
 #include <SDL2/SDL.h>
-#include <bitset>
-#include <iostream>
 
-#define CONTROLLER_A_MASK ((uint8_t) 0x80)
-#define CONTROLLER_B_MASK ((uint8_t) 0x40)
-#define CONTROLLER_SELECT_MASK ((uint8_t) 0x20)
-#define CONTROLLER_START_MASK ((uint8_t) 0x10)
-#define CONTROLLER_UP_MASK ((uint8_t) 0x08)
-#define CONTROLLER_DOWN_MASK ((uint8_t) 0x04)
-#define CONTROLLER_LEFT_MASK ((uint8_t) 0x02)
-#define CONTROLLER_RIGHT_MASK ((uint8_t) 0x01)
+Controller::Controller(Console* console) : console(console) {}
 
-Controller::Controller(Console* console)
-    : console(console), controller_state(0) {}
-
-uint8_t Controller::getState() const { return controller_state; }
-
-void Controller::updateController() {
-    const Uint8* keystate = SDL_GetKeyboardState(NULL);
+uint8_t Controller::getState() {
+    const Uint8* keystate = SDL_GetKeyboardState(nullptr);
 
     uint8_t new_state = 0;
 
-    if (keystate[SDL_SCANCODE_Z])
-        new_state |= CONTROLLER_A_MASK;
-    if (keystate[SDL_SCANCODE_X])
-        new_state |= CONTROLLER_B_MASK;
-    if (keystate[SDL_SCANCODE_LSHIFT] || keystate[SDL_SCANCODE_RSHIFT])
-        new_state |= CONTROLLER_SELECT_MASK;
-    if (keystate[SDL_SCANCODE_RETURN])
-        new_state |= CONTROLLER_START_MASK;
-    if (keystate[SDL_SCANCODE_UP])
-        new_state |= CONTROLLER_UP_MASK;
-    if (keystate[SDL_SCANCODE_DOWN])
-        new_state |= CONTROLLER_DOWN_MASK;
-    if (keystate[SDL_SCANCODE_LEFT])
-        new_state |= CONTROLLER_LEFT_MASK;
-    if (keystate[SDL_SCANCODE_RIGHT])
-        new_state |= CONTROLLER_RIGHT_MASK;
+    if (keystate[SDL_SCANCODE_Z] != 0)
+        new_state |= static_cast<uint8_t>(Mask::A);
+    if (keystate[SDL_SCANCODE_X] != 0)
+        new_state |= static_cast<uint8_t>(Mask::B);
+    if ((keystate[SDL_SCANCODE_LSHIFT] != 0) ||
+        (keystate[SDL_SCANCODE_RSHIFT] != 0))
+        new_state |= static_cast<uint8_t>(Mask::SELECT);
+    if (keystate[SDL_SCANCODE_RETURN] != 0)
+        new_state |= static_cast<uint8_t>(Mask::START);
+    if (keystate[SDL_SCANCODE_UP] != 0)
+        new_state |= static_cast<uint8_t>(Mask::UP);
+    if (keystate[SDL_SCANCODE_DOWN] != 0)
+        new_state |= static_cast<uint8_t>(Mask::DOWN);
+    if (keystate[SDL_SCANCODE_LEFT] != 0)
+        new_state |= static_cast<uint8_t>(Mask::LEFT);
+    if (keystate[SDL_SCANCODE_RIGHT] != 0)
+        new_state |= static_cast<uint8_t>(Mask::RIGHT);
 
-
-    if (new_state != controller_state) {
-        controller_state = new_state;
-        console->memory.w8u(0x7000, new_state);
-        std::cerr << "Wrote in mem add" << (uint16_t) new_state << std::endl;
-        displayControllerState();
-    }
-}
-
-void Controller::displayControllerState() const {
-    std::cout << std::bitset<8>(controller_state) << std::endl;
+    return new_state;
 }
--- src/controller.h
+++ src/controller.h
@@ -2,26 +2,25 @@
 
 #include <cstdint>
 
-#define CONTROLLER_A_MASK ((uint8_t) 0x80)
-#define CONTROLLER_B_MASK ((uint8_t) 0x40)
-#define CONTROLLER_SELECT_MASK ((uint8_t) 0x20)
-#define CONTROLLER_START_MASK ((uint8_t) 0x10)
-#define CONTROLLER_UP_MASK ((uint8_t) 0x08)
-#define CONTROLLER_DOWN_MASK ((uint8_t) 0x04)
-#define CONTROLLER_LEFT_MASK ((uint8_t) 0x02)
-#define CONTROLLER_RIGHT_MASK ((uint8_t) 0x01)
-
 
 class Console;
 
 class Controller {
+    enum class Mask : uint8_t {
+        A = 0x80,
+        B = 0x40,
+        SELECT = 0x20,
+        START = 0x10,
+        UP = 0x08,
+        DOWN = 0x04,
+        LEFT = 0x02,
+        RIGHT = 0x01,
+    };
+
   public:
     Controller(Console* console);
-    uint8_t getState() const;
-    void updateController();
-    void displayControllerState() const;
+    [[nodiscard]] static uint8_t getState();
 
   private:
     Console* console;
-    uint8_t controller_state = 0;
 };
