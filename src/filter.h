f69ca6814022702c2447660f8c91d144d728dad5
Monisha <Monisha36912@gmail.com>
2025-03-07 13:11:32 -0800

    code

---
+++ src/filter.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+class Filter {
+public:
+    // Apply a background color filter to the pixels
+    // This will set all pixels to the specified red, green, and blue values
+    static void applyBackgroundColorFilter(std::array<uint32_t, 65536>& pixels, uint8_t red, uint8_t green, uint8_t blue);
+
+private:
+    // Prevent instantiation of the Filter class
+    Filter() = default;
+};
