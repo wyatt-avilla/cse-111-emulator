9ec62bfe3dd53177881c607fe775d02db7ea9f64
Monisha <Monisha36912@gmail.com>
2025-03-09 13:30:52 -0700

    intial color

--- src/gpu.cpp
+++ src/gpu.cpp
@@ -15,7 +15,7 @@ const int WINDOW_WIDTH = GPU::FRAME_WIDTH * WINDOW_SCALE;
 const int WINDOW_HEIGHT = GPU::FRAME_HEIGHT * WINDOW_SCALE;
 
 GPU::GPU(Console* console)
-    : console(console), window(nullptr), renderer(nullptr), texture(nullptr), selectedColorMod() {}
+    : console(console), window(nullptr), renderer(nullptr), texture(nullptr), selectedColorMod{0, 128, 0} {}
 
 GPU::~GPU() {
     if (texture != nullptr) {
