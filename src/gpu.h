5a7ec843ac2fc1bc9a6dd4e4303fac7b239681b0
Monisha <Monisha36912@gmail.com>
2025-03-09 14:11:05 -0700

    debug

--- src/gpu.h
+++ src/gpu.h
@@ -33,9 +33,7 @@ class GPU {
 
     void setSelectedColor(uint8_t r, uint8_t g, uint8_t b);
   private:
-    struct Color {
-      uint8_t r, g, b;
-  };
+    
 
     Console* console;
 
@@ -48,7 +46,7 @@ class GPU {
     uint8_t* external_vram = nullptr;
 
     VideoRecorder* video_recorder = nullptr;
-    Color selectedColorMod; 
+    SDL_Color selectedColorMod; 
     
 };
 
